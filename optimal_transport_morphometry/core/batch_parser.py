4e1e323b7221d0a72e4eefd733aad9d72e78f26c
Zach Mullen <zach.mullen@kitware.com>
2020-11-23 13:35:52 -0500

    Use bulk insert into database during CSV ingest

--- optimal_transport_morphometry/core/batch_parser.py
+++ optimal_transport_morphometry/core/batch_parser.py
@@ -11,14 +11,20 @@ from optimal_transport_morphometry.core import models
 def load_batch_from_csv(csvfile: TextIO, dest: models.Dataset) -> models.UploadBatch:
     reader = csv.DictReader(csvfile)
     batch = models.UploadBatch.objects.create(dataset=dest)
+    patients = []
+    uploads = []
 
     for row in reader:
         expected_name = row.pop('name')
         patient_id = row.pop('patient', '').strip() or uuid4().hex
         # TODO allow using existing patients too
-        patient = models.Patient.objects.create(identifier=patient_id)
-        models.PendingUpload.objects.create(
-            batch=batch, patient=patient, name=expected_name, metadata=row
+        patient = models.Patient(identifier=patient_id)
+        patients.append(patient)
+        uploads.append(
+            models.PendingUpload(batch=batch, patient=patient, name=expected_name, metadata=row)
         )
+
+    models.Patient.objects.bulk_create(patients)
+    models.PendingUpload.objects.bulk_create(uploads)
 
     return batch
