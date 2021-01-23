c08247ff0ae1e08f8d717a718a747bd7000d7a92
Zach Mullen <zach.mullen@kitware.com>
2021-01-23 11:16:34 -0500

    Add timestamps to preprocessing artifact models

--- optimal_transport_morphometry/core/models/segmented_image.py
+++ optimal_transport_morphometry/core/models/segmented_image.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 from s3_file_field import S3FileField
 
 from .atlas import Atlas
 from .image import Image
 
 
-class SegmentedImage(models.Model):
+class SegmentedImage(TimeStampedModel):
     blob = S3FileField()
     source_image = models.ForeignKey(
         Image,
