eaf3dd71f51367fbe84f40004bfa8426262b0871
Zach Mullen <zach.mullen@kitware.com>
2021-06-18 12:34:53 -0400

    Infer atlases if not passed for preprocessing

--- optimal_transport_morphometry/core/rest/preprocess.py
+++ optimal_transport_morphometry/core/rest/preprocess.py
@@ -9,15 +9,29 @@ from optimal_transport_morphometry.core.tasks import preprocess_images
 
 
 class PreprocessSerializer(serializers.Serializer):
-    atlas = serializers.IntegerField()
-    atlas_csf = serializers.IntegerField()
-    atlas_grey = serializers.IntegerField()
-    atlas_white = serializers.IntegerField()
+    atlas = serializers.IntegerField(required=False)
+    atlas_csf = serializers.IntegerField(required=False)
+    atlas_grey = serializers.IntegerField(required=False)
+    atlas_white = serializers.IntegerField(required=False)
     dataset = serializers.IntegerField()
 
 
 class PreprocessResponseSerializer(serializers.Serializer):
     task_id = serializers.CharField()
+
+
+def _load_atlas(serializer, key: str) -> Atlas:
+    passed_id = serializer.validated_data.get(key)
+    if passed_id:
+        return get_object_or_404(Atlas, pk=passed_id)
+
+    name = {
+        'atlas': 'T1.nii',
+        'atlas_csf': 'csf.nii',
+        'atlas_grey': 'grey.nii',
+        'atlas_white': 'white.nii',
+    }[key]
+    return get_object_or_404(Atlas, name=name)
 
 
 class PreprocessingViewSet(GenericViewSet):
@@ -31,20 +45,16 @@ class PreprocessingViewSet(GenericViewSet):
         serializer.is_valid(raise_exception=True)
 
         # Validate that the IDs are good before we invoke the celery task
-        atlas_id = serializer.validated_data['atlas']
-        atlas_csf_id = serializer.validated_data['atlas_csf']
-        atlas_grey_id = serializer.validated_data['atlas_grey']
-        atlas_white_id = serializer.validated_data['atlas_white']
+        atlas = _load_atlas(serializer, 'atlas')
+        atlas_csf = _load_atlas(serializer, 'atlas_csf')
+        atlas_grey = _load_atlas(serializer, 'atlas_grey')
+        atlas_white = _load_atlas(serializer, 'atlas_white')
         dataset_id = serializer.validated_data['dataset']
         # TODO get better error messages than what get_object_or_404 provides us
-        get_object_or_404(Atlas, pk=atlas_id)
-        get_object_or_404(Atlas, pk=atlas_csf_id)
-        get_object_or_404(Atlas, pk=atlas_grey_id)
-        get_object_or_404(Atlas, pk=atlas_white_id)
         get_object_or_404(Dataset, pk=dataset_id)
 
         task = preprocess_images.delay(
-            atlas_id, atlas_csf_id, atlas_grey_id, atlas_white_id, dataset_id
+            atlas.id, atlas_csf.id, atlas_grey.id, atlas_white.id, dataset_id
         )
         serializer = PreprocessResponseSerializer({'task_id': task.id})
         return Response(serializer.data)
