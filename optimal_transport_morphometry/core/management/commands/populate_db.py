bfac6b32a4cfd35d8eb96abb03686a9a4a19bd81
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-07-14 17:13:07 -0400

    Add owner option to populate_db command

--- optimal_transport_morphometry/core/management/commands/populate_db.py
+++ optimal_transport_morphometry/core/management/commands/populate_db.py
@@ -1,6 +1,8 @@
 from pathlib import Path
-from typing import List, TextIO
+from typing import List, Optional, TextIO
 
+from click import ClickException
+from django.contrib.auth.models import User
 from django.core.files import File
 from django.core.files.uploadedfile import SimpleUploadedFile
 import djclick as click
@@ -20,10 +22,15 @@ images_dir = root_dir / 'sample_data' / 'images'
 
 @click.command()
 @click.argument('csv', type=click.File())
+@click.option('--owner', 'email', required=True, help='The email of the dataset owner.')
 @click.option('--dataset-name', default='Test dataset', help='The name of the dataset to create.')
 @click.option('--clear', is_flag=True, help='Clear all existing upload batches.')
 @click.option('--include-images', is_flag=True, help='Include upload of corresponding images.')
-def command(csv: TextIO, dataset_name: str, clear: bool, include_images: bool) -> None:
+def command(csv: TextIO, email: str, dataset_name: str, clear: bool, include_images: bool) -> None:
+    owner: Optional[User] = User.objects.filter(email=email).first()
+    if owner is None:
+        raise ClickException(f'Owner not found with email: {owner}')
+
     for name in ['grey', 'white', 'csf', 'T1']:
         if not Atlas.objects.filter(name=f'{name}.nii').exists():
             print(f'Uploading {name} atlas')
@@ -36,7 +43,7 @@ def command(csv: TextIO, dataset_name: str, clear: bool, include_images: bool) -
 
     # Add dataset and pending upload batch
     print('Ingesting upload batch and pending upload...')
-    dataset, _ = Dataset.objects.get_or_create(name=dataset_name)
+    dataset, _ = Dataset.objects.get_or_create(name=dataset_name, owner=owner)
     batch = load_batch_from_csv(csv, dest=dataset)
 
     # Automatically ingest images if desired
