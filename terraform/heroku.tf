90a7a1f3ae24d3ee9c164f7a78e4d7e6011c54c1
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-08-01 15:48:22 -0400

    Add ec2 worker

--- terraform/heroku.tf
+++ terraform/heroku.tf
@@ -13,4 +13,10 @@ module "django" {
 
   heroku_postgresql_plan      = "hobby-basic"
   heroku_worker_dyno_quantity = 0
+
+  # worker
+  ec2_worker_instance_quantity = 1
+  ec2_worker_instance_type     = "r5a.2xlarge"
+  ec2_worker_ssh_public_key    = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILvUQxv4YjuI67JMxxsH+Cq5puATcZKhNf5RbryMo9ui jjnesbitt2@gmail.com"
+  ec2_worker_volume_size       = "64"
 }
