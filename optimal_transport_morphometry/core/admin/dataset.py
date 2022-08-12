6b7345efba8a0386d2b9f30648e2df1a08c6c473
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-08-12 13:24:37 -0400

    Expose missing dataset fields in admin ui

--- optimal_transport_morphometry/core/admin/dataset.py
+++ optimal_transport_morphometry/core/admin/dataset.py
@@ -6,11 +6,11 @@ from optimal_transport_morphometry.core.models import Dataset
 
 @admin.register(Dataset)
 class DatasetAdmin(GuardedModelAdmin):
-    list_display = ['id', 'name', 'owner']
+    list_display = ['id', 'name', 'owner', 'public', 'preprocessing_status', 'analysis_status']
     list_display_links = ['id', 'name']
     list_filter = ['name']
     list_select_related = True
 
     search_fields = ['name']
 
-    fields = ['name', 'description', 'owner', 'public']
+    fields = ['name', 'description', 'owner', 'public', 'preprocessing_status', 'analysis_status']
