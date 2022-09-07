2bdb9c1da9878cf640cfec7c1847925a861f0a45
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-09-07 13:39:36 -0400

    Update terraform girder4 module

--- terraform/heroku.tf
+++ terraform/heroku.tf
@@ -3,14 +3,17 @@ data "heroku_team" "heroku" {
 }
 
 module "django" {
-  source  = "girder/django/heroku"
-  version = "0.8.0"
+  source  = "girder/girder4/heroku"
+  version = "0.11.0"
 
+  heroku_team_name = data.heroku_team.heroku.name
   project_slug     = "kitware-otm"
   route53_zone_id  = data.aws_route53_zone.kitware_otm.zone_id
-  heroku_team_name = data.heroku_team.heroku.name
   subdomain_name   = "app"
 
-  heroku_postgresql_plan      = "hobby-basic"
-  heroku_worker_dyno_quantity = 0
+  heroku_postgresql_plan             = "standard-0"
+  heroku_cloudamqp_plan              = "squirrel-1"
+  heroku_worker_dyno_quantity        = 0
+  django_cors_origin_whitelist       = ["https://otm-client.pages.dev"]
+  django_cors_origin_regex_whitelist = ["^https:\\/\\/[0-9a-z]+\\.otm-client.pages.dev$"]
 }
