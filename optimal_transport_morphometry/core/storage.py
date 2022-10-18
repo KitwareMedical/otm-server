3d6f604c085f69ff9fce5404f69326718b1e12a0
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-10-18 12:59:27 -0400

    Fix conditional storage imports

--- optimal_transport_morphometry/core/storage.py
+++ optimal_transport_morphometry/core/storage.py
@@ -5,12 +5,22 @@ import boto3
 import botocore
 from django.conf import settings
 from django.core.files.storage import get_storage_class
-from minio_storage.storage import MinioStorage
 from s3_file_field import S3FileField
-from storages.backends.s3boto3 import S3Boto3Storage
 
 if TYPE_CHECKING:
     from mypy_boto3_s3.client import S3Client
+
+# Import storages
+try:
+    from storages.backends.s3boto3 import S3Boto3Storage
+except ImportError:
+    # This should only be used for type interrogation, never instantiation
+    S3Boto3Storage = type('FakeS3Boto3Storage', (), {})
+try:
+    from minio_storage.storage import MinioStorage
+except ImportError:
+    # This should only be used for type interrogation, never instantiation
+    MinioStorage = type('FakeMinioStorage', (), {})
 
 
 def get_boto_client(config: Optional[botocore.client.Config] = None) -> 'S3Client':
